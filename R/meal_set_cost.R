9e266fb8b3091f59085492dc37a2981205e3efb1
a-ramji <anna@ramji.org>
2023-08-20 15:31:00 -0700

    added description info to package and meal set calculator .R

--- R/meal_set_cost.R
+++ R/meal_set_cost.R
@@ -7,15 +7,15 @@
 #' @return
 #' @export
 #'
-#' @examples
+#' @examples Meal Set A: Tonkatsu (with sauce) over rice with unlimited refills for sides of coleslaw and small soup of the day. Meal Set B: Your choice of grilled chicken or baked tofu, tossed with soy-garlic glaze, sweet corn, and zucchini over rice. Optional: crispy dried seaweed flakes and sesame seeds. Also comes with melted mozzarella cheese on top.
 meal_set_cost <- function(set_name, count) {
   if (set_name == "A") {
     price_a <- count * 12.00
-    print(paste0("Your total is $", price_a))
+    print(paste0("Your total is $", price_a, "! Havea nice day!"))
   }
   else if (set_name == "B") {
-    price_b <- count * 15
-    print(paste0("Your total is $", price_b))
+    price_b <- count * 15.00
+    print(paste0("Your total is $", price_b, "! Havea nice day!"))
   }
   else
     print("I'm sorry, only A and B are meal set options today")
